3444a9b5e4e87ee2f60bd992b1190ace78acd446
Asahi <161689859+6Asahi9@users.noreply.github.com>
2026-03-15 21:04:15 +0530

    render1.0

--- src/Home/Intro.jsx
+++ src/Home/Intro.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/Images/logo.png";
 import backgroundImage from "../assets/Images/BigBackground.jpg";
 import "./Intro.css";
 
